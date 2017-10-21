0d50fdba2d09571d87dc207743d1c919b7b485bd
Diane deGraft-Johnson <dddegraft@gmail.com>
2017-10-20 21:04:31 -0700

    Add files via upload

---
+++ rvalue_refs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+// Have some examples that highlight if not using Rvalue Refs
+
+int main(int argc, char **argv)
+{
+    std::cout << "Rvalue References" << std::endl
+              << std::endl;
+
+    return 0;
+}
